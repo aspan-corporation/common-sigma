6f89e11991cfcd168ed4e21cb6adba5b5957ecc7
Adil Nurtayev <anurtayev@torstar.ca>
2022-04-09 11:04:23 -0400

    created dynamoTableRipper

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,47 @@
 import { extname } from "path";
+import { info } from "console";
+import { documentClient } from "./awsClients";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
 export const isKeyExtensionAllowed = (ext: string): boolean =>
   process.env.ALLOWED_EXTENSIONS.split("|").includes(ext);
 
 export const getExtension = (key: string): string =>
   extname(key).toLowerCase().slice(1);
+
+
+export const dynamoTableRipper = async ({
+  tableName,
+  processorFn,
+}: {
+  tableName: string;
+  processorFn: (items: DocumentClient.ItemList) => Array<any>;
+}) => {
+  let result: Array<any> = [];
+  let isFetching = true;
+  let lastEvaluatedKey: DocumentClient.Key;
+
+  do {
+    const { LastEvaluatedKey, Items, Count } = await documentClient
+      .scan({
+        TableName: tableName,
+        ...(lastEvaluatedKey ? { ExclusiveStartKey: lastEvaluatedKey } : {}),
+      })
+      .promise();
+
+    if (Count) result.concat(processorFn(Items));
+
+    info(
+      `fetched: Count - ${Count}, LastEvaluatedKey - ${JSON.stringify(
+        LastEvaluatedKey,
+        null,
+        2
+      )}`
+    );
+
+    lastEvaluatedKey = LastEvaluatedKey;
+    isFetching = Boolean(LastEvaluatedKey);
+  } while (isFetching);
+
+  return result;
+};
