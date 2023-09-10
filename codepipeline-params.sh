79dfcac2e0655a96bb3382a3b144b696bde95e64
Adil Nurtayev <anurtayev@gmail.com>
2023-09-10 18:50:18 -0400

    fix: modified BRANCH_HASH algo

--- codepipeline-params.sh
+++ codepipeline-params.sh
@@ -9,7 +9,7 @@ export ORGANIZATION=`git remote show origin | head -n 2 | tail -n 1 | awk -F '/'
 export REPOSITORY=`git remote show origin | head -n 2 | tail -n 1 | awk -F '/' '{print $5}' | awk -F '.' '{print $1}'`
 export BRANCH=`git rev-parse --abbrev-ref HEAD`
 export REGION=`aws configure get region`
-export BRANCH_HASH=`node -e "console.log(crypto.createHash('shake256', {outputLength: 6}).update('${BRANCH}').digest('base64'))"`
+export BRANCH_HASH=`node -e "console.log(crypto.createHash('shake256', {outputLength: 5}).update('${BRANCH}').digest('hex'))"`
 
 echo "Parameters values:"
 
