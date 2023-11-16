240647d935d0a581dfc287b58fbc95d732993335
Adil Nurtayev <adil.nurtayev@clevertech.biz>
2023-11-16 00:54:46 -0500

    fix: renamed pipeline stack

--- codepipeline-create.sh
+++ codepipeline-create.sh
@@ -3,7 +3,7 @@
 . ./codepipeline-params.sh
 
 sam deploy -t codepipeline.yml \
-    --stack-name $REPOSITORY-bootstrap \
+    --stack-name $REPOSITORY-pipeline \
     --region $REGION \
     --capabilities=CAPABILITY_IAM \
     --parameter-overrides="FullRepositoryId=${ORGANIZATION}/${REPOSITORY} BranchName=${BRANCH} Region=${REGION} ComponentStackName=${REPOSITORY}"
